d3d17b1eb365f019e4ba8624889b3f957eb079e7
zdhmf <zdhmf@users.noreply.github.com>
2026-06-11 10:33:06 +0800

    fix: enable swagger docs setup

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,6 +3,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { setupSwagger } from './swagger.setup';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,7 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.useGlobalFilters(new HttpExceptionFilter());
+  setupSwagger(app);
 
   await app.listen(4000);
 }
